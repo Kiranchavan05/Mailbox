9b75689c30297afb0de5fa4d6a3d0a9a21f2e1c5
kiran chavan <125879292+Kiranchavan05@users.noreply.github.com>
2023-06-06 08:23:09 +0530

    MailBox task1 signup screen

---
+++ App.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+import './App.css';
+import Login from './component/Login';
+import Header from './component/Header';
+
+function App() {
+ return <Fragment>
+   <Header />
+    <Login /> 
+ </Fragment>
+}
+
+export default App;
