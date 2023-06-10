1cf9c4f652cb1f8f096b29c41129fff8ca4ecff5
kiran chavan <125879292+Kiranchavan05@users.noreply.github.com>
2023-06-10 13:15:16 +0530

    Read Messages properly

--- Inbox.js
+++ Inbox.js
@@ -1,58 +1,94 @@
 import React from "react";
-import { useState } from "react";
 import { useEffect } from "react";
-import { Container, ListGroup } from "react-bootstrap";
-import { useSelector } from "react-redux";
+import { Container, ListGroup, Modal } from "react-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
+import { inboxActions } from "./InboxSlice";
+import { fetchData } from "./InboxSlice";
+import { updateData } from "./InboxSlice";
+
 
 const Inbox = () => {
   const userEmail = useSelector((state) => state.auth.userEmail);
   const email = userEmail.replace(/[@.]/g, "");
-  const [mails, setMails] = useState([]);
+  const mails = useSelector((state) => state.inbox.mails);
+  const dispatch = useDispatch();
+  const unReadMails = useSelector((state) => state.inbox.unreadCount);
+  const selectedEmail = useSelector(state => state.inbox.selectedEmail)
 
-  useEffect(() => {
-    const fetchMails = async () => {
-      try {
-        const response = await fetch(
-          `https://mailbox-2f973-default-rtdb.firebaseio.com/${email}/inbox.json`
-        );
-        if (response.ok) {
-          const data = await response.json();
-          setMails(data);
-        } else {
-          throw new Error("something went wrong");
-        }
-      } catch (err) {
-        alert("something went wrong");
-      }
-    };
+  useEffect(()=> {
+    dispatch(fetchData(email))
+  }, [email, dispatch])
 
-    fetchMails();
-  }, []);
+  const openMail = (key) => {
+    localStorage.setItem('key clicked', key)
+      dispatch(inboxActions.setReadToTrue())
+      dispatch(inboxActions.setSelectedEmail(mails[key]))
+      dispatch(updateData(key, email))
+  }
 
-  console.log(mails);
+ 
+  const closeModal = () => {
+    dispatch(inboxActions.setSelectedEmail(null))
+  }
 
   return (
+    <div>
     <Container className="mt-5">
       <h1 className="">INBOX</h1>
+      <h3 className="text-center text-danger">{`Total Number of unread Mails : ${unReadMails}`}</h3>
       <ListGroup>
         {Object.keys(mails).map((key) => (
           <ListGroup.Item
             key={key}
             className="m-2"
             style={{
-              backgroundColor: '#a563',
+              backgroundColor: "aquamarine",
               border: "1px solid blue",
               borderRadius: "5px",
-              display : 'flex',
-              justifyContent : 'space-between'
+              display: "flex",
+              justifyContent: "flex-start",
             }}
           >
-            {`${mails[key].from} - ${mails[key].subject} - ${mails[key].content}`}{" "}
+            <div onClick={() => openMail(key)}>
+              {!mails[key].read && (
+                <div
+                  style={{
+                    display: "inline-block",
+                    width: "0.5rem",
+                    height: "0.5rem",
+                    borderRadius: "50%",
+                    backgroundColor: "blue",
+                    marginRight: "20px",
+                  }}
+                />
+              )}
+              {`${mails[key].from} - ${mails[key].subject} - ${mails[key].content} - ${mails[key].read}`}
+            </div>
           </ListGroup.Item>
         ))}
       </ListGroup>
     </Container>
-  );
+    <Modal show={selectedEmail !== null} onHide={closeModal}>
+      <Modal.Header closeButton>
+        <Modal.Title>Email details</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>
+          <strong>From :</strong>
+          {selectedEmail && selectedEmail.from}
+        </p>
+        <p>
+          <strong>subject : </strong>
+          {selectedEmail && selectedEmail.subject}
+        </p>
+        <p>
+          <strong>Content :</strong>
+          {selectedEmail && selectedEmail.content}
+        </p>
+      </Modal.Body>
+    </Modal>
+  </div>
+);
 };
 
 export default Inbox;
